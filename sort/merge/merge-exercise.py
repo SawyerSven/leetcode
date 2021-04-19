163a902dd54867a505201f8c16217b897e7278ce
sunsiyuan <siyuan.sun1@baozun.com>
2021-04-19 17:40:23 +0800

    refactor: 优化merge-exercise

--- sort/merge/merge-exercise.py
+++ sort/merge/merge-exercise.py
@@ -5,10 +5,10 @@ def merge(front, end):
             temp.append(front.pop(0))
         else:
             temp.append(end.pop(0))
-    while(len(front)):
-        temp.append(front.pop(0))
-    while(len(end)):
-        temp.append(end.pop(0))
+    if(len(front)):
+        temp.extend(front)
+    if(len(end)):
+        temp.extend(end)
     return temp
 
 
@@ -21,6 +21,6 @@ def mergeSort(arr):
     return merge(mergeSort(start), mergeSort(end))
 
 
-arr = [5, 8, 1, 3, 7, 4, 2, 6, 9]
+arr = [5, 8, 1, 3, 7, 4, 2, 6, 9, 2, 2, 3, 4, 5, 12, 312, 415, 124, 123]
 
 print(mergeSort(arr))
